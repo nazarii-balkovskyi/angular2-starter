050ef2343c4849b89fa4b9c82f8021c38a3fbbec
Nazarii Balkovskyi <Nazarii Balkovskyi>
2016-06-29 07:23:19 +0300

    Updated angular dependencies version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { provideRouter, RouterConfig } from '@angular/router';
+import {ServiceDemoComponent} from "./components/service-demo/service-demo.component";
 
 export const routes: RouterConfig = [
-    { path: '', component: RouterConfig }
+    { path: '', component: ServiceDemoComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
---
+++ src/app/components/service-demo/service-demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'service-demo',
+    template: require('./service-demo.component.html')
+})
+export class ServiceDemoComponent {
+}
